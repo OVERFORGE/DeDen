53564a7e6ccef5a3d142ed31ddb1f5681ea26a89
OVERFORGE <dkdakshkhurana@gmail.com>
2025-11-13 00:28:48 +0530

    font fixed

--- frontend/app/fonts.ts
+++ frontend/app/fonts.ts
@@ -1,7 +1,7 @@
 import localFont from "next/font/local";
 
 export const berlin = localFont({
-  src: "../public/fonts/berlin.ttf",
+  src: "./fonts/berlin.ttf",
   weight: "600",
   style: "normal",
   variable: "--font-berlin",
